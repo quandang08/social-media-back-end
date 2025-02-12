db23304f4f96defb72705391ef7e5d0e5288e746
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-02-12 16:26:55 +0700

    Add Like model to link users and twits

---
+++ social_media_backend/src/main/java/com/socialmedia/backend/model/Like.java
@@ -0,0 +1,19 @@
+package com.socialmedia.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Twit twit;
+}
