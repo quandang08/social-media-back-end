8ba9616a4fb6f23e09704414017321e5cfea2791
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-03-18 10:27:05 +0700

    Exception

---
+++ social_media_backend/src/main/java/com/socialmedia/backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.socialmedia.backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
